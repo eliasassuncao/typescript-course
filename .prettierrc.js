474df24781415f40979d74b2a398aee0edf61d81
eliasassuncao <elias3748@gmail.com>
2024-02-26 10:03:11 -0300

    feat: ts lint config

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuota: true,
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: 'auto'
+}
---
+++ index.ts
@@ -0,0 +1,3 @@
+const test = "xD"
+
+console.log(test)
